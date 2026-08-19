1152a58b24e4055e23ef180d87e98cb3ad78d5cc
Olivier Louvignes <olivier@mgcrea.io>
2026-08-19 17:02:42 +0200

    feat(app): scaffold Cupertino macOS app shell

---
+++ app/Cupertino/CupertinoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupertinoApp.swift
+//  Cupertino
+//
+//  Created by Olivier Louvignes on 19/08/2026.
+//
+
+import SwiftUI
+
+@main
+struct CupertinoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
